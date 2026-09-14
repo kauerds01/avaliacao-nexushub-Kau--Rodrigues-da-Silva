8efb82c2d9e8691b5166d82d680c6d02f05bebad
kauerds01 <rodrigueskaue087@gmail.com>
2026-09-14 11:24:23 +0000

    V1

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+#Bloco Dicionario/Lista
+startup = {
+    "nome":"CyberPulse Tech",
+    "segmento":"Segurança da Informação",
+    "ano_adesao":2026
+}
+solucoes_ativas = ["Firewall IA", "Scan de Vulnerabilidades"]
+print("STARTUP===📊",startup["nome"],"===")
+print("SEGMENTO===",startup["segmento"],"===")
+print("PRODUTO===",solucoes_ativas[0])
 
+#Bloco Matriz
+bancadas = [
+    ["Bancada N1 (1)","Bancada N2 (0)"],
+    ["Bancada S1 (0)", "Bancada S2 (1)"]
+]
+print("A seguir teremos a ocupação atual da bancadas sendo:\n 1:Ocupado\n 0:livre")
+print(bancadas[0][0])
+print(bancadas[0][1])
+print(bancadas[1][0]) 
+print(bancadas[1][1])
