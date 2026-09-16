d7d5c5f7c6611fec97abdf5bd93ec80f6636f8a0
kauerds01 <rodrigueskaue087@gmail.com>
2026-09-16 13:06:50 +0000

    V2

--- main.py
+++ main.py
@@ -19,3 +19,30 @@ print(bancadas[0][0])
 print(bancadas[0][1])
 print(bancadas[1][0]) 
 print(bancadas[1][1])
+
+with open("custos_cloud.csv", "r", encoding="utf-8") as arquivo:
+    linha_cabecalho = arquivo.readline()
+    linha1 = arquivo.readline()
+    linha2 = arquivo.readline()
+    linha3 = arquivo.readline()
+    linha4 = arquivo.readline()
+
+print(linha_cabecalho)
+print(linha1)
+print(linha2)
+print(linha3)
+print(linha4)
+#Etapa 4----
+
+nome_startup = startup["nome"]
+bancada_alocada = "Bancada N1"
+custo1 = float(linha1.split(",")[1].strip())
+custo2 = float(linha2.split(",")[1].strip())
+custo3 = float(linha3.split(",")[1].strip())
+custo4 = float(linha4.split(",")[1].strip())
+valor_total = custo1 + custo2 + custo3 + custo4
+
+print("\n=== PAINEL DE CONSOLIDAÇÃO ===")
+print(f"Startup: {nome_startup}")
+print(f"Bancada alocada: {bancada_alocada}")
+print(f"Custo total de infraestrutura Cloud: R$ {valor_total:.2f}")
